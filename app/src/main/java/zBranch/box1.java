8b3a738f764617bd8c6599e0b1b3277f6a94dd26
Your Name <zobayerabedin@gmail.com>
2023-03-28 02:52:35 +0600

    modify from master

--- app/src/main/java/zBranch/box1.java
+++ app/src/main/java/zBranch/box1.java
@@ -1,25 +1,37 @@
 package zBranch;
 
 public class box1 {
-    int h, w;
-    box1(int h, int w){
+    int h, w, A;
+
+    public box1(int h, int w, int a) {
         this.h = h;
         this.w = w;
-    }
-
-    public void setW(int w) {
-        this.w = w;
+        A = a;
     }
 
     public void setH(int h) {
         this.h = h;
     }
 
-    public int getW() {
-        return w;
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public void setA(int a) {
+        A = a;
     }
 
     public int getH() {
         return h;
     }
+
+    public int getW() {
+        return w;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+
 }
