8683015c785dbd628d991b43b3911853cf911b20
Your Name <zobayerabedin@gmail.com>
2023-03-28 02:27:24 +0600

    class added

---
+++ app/src/main/java/assignment1/box.java
@@ -0,0 +1,27 @@
+package assignment1;
+
+public class box {
+    int h, w;
+    box(int h, int w) {
+        this.h = h;
+        this.w = w;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+
+}
